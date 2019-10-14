98d4b373fa313c63ba7343e715df153f778f124f
westgoten <rodrigodeoliveira397@gmail.com>
2019-10-14 18:03:48 -0300

    Started repo

---
+++ source/ruby-on-rails-latest-commits/app/src/main/java/com/westgoten/ruby_on_rails_latest_commits/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.westgoten.ruby_on_rails_latest_commits
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
